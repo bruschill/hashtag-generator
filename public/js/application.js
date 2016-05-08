cba2a693d6560a16f2e5465b4b108f98cf1a79c8
Hunter Braun <hunter.braun@gmail.com>
2016-05-08 01:24:13 -0500

    Finish JS refactor

--- public/js/application.js
+++ public/js/application.js
@@ -9,10 +9,10 @@
     }, 500);
   }
 
-  function generateHashtag() {
+  function generateHashtag(text) {
     //make sure textToConvert is a non-empty string
-    if(!!$('#user-input')) {
-      var textToConvert = $('#user-input').val();
+    if(text) {
+      var textToConvert = text;
 
       var splitTextToConvert, capitalCamelCasedString;
       var capitalizedWords = [];
@@ -24,7 +24,7 @@
       splitTextToConvert = textToConvert.split(' ');
       //iterate through each element (word), capitalize the first character,
       //then push it on to end of capitalizedWords array
-      for(var i = 0; i < splitTextToConvert.length; i++) {
+      for (var i = 0; i < splitTextToConvert.length; i++) {
         var word = splitTextToConvert[i];
 
         word = word.substr(0, 1).toUpperCase() + word.substring(1).toLowerCase();
@@ -36,42 +36,70 @@
 
       //prepend an octothorpe to capitalCamelCasedString and return it
       var finalHashtag = "#" + capitalCamelCasedString;
-
-      $('#hashtag').text(finalHashtag);
+      return finalHashtag;
     }
+  }
+
+  function runAction() {
+    $('#user-input').val() ? showHashtagDisplay() : showError();
+  }
+
+  function showError() {
+    $('#user-input').addClass('error');
   }
 
   function showHashtagDisplay() {
     var hashtagElement = $('#hashtag');
     var button = $('#main-btn');
+    var input = $('#user-input');
+    var myConfetti = $('#confetti');
 
+    //restore to default state
+    input.removeClass('error');
+    clearTimeout(confettiTimerID);
+    myConfetti.css('opacity', 1);
     flashScreen();
 
     window.confetti.start();
-    $('#confetti').show();
+    myConfetti.show();
 
     confettiTimerID = setTimeout(function() {
-      $('#confetti').hide();
-      window.confetti.stop();
+      myConfetti.animate({
+        opacity: 0
+      }, 1000, function() {
+        myConfetti.hide();
+        window.confetti.stop();
+      });
     }, 4000);
 
     if (button.prop('value') == 'Generate') {
       button.prop('value', 'Try again?');
-      $('#user-input').css('visibility', 'hidden');
-      generateHashtag();
+      input.css('visibility', 'hidden');
+
+      $('#hashtag').text(generateHashtag(input.val()));
       hashtagElement.css('visibility', 'visible');
     } else {
-      $('#confetti').hide();
+      myConfetti.hide();
       window.confetti.stop();
+
       button.prop('value', 'Generate');
-      $('#user-input').css('visibility', 'visible');
+      input.css('visibility', 'visible');
       hashtagElement.css('visibility', 'hidden');
-      $('#user-input').val('');
-      clearTimeout(confettiTimerID);
+      input.val('');
     };
   }
 
   $(document).ready(function() {
-    $('#main-btn').click(showHashtagDisplay);
+    var input = $('#user-input');
+
+    input.val('');
+    input.bind('enterKey', runAction);
+    input.keydown(function(e) {
+      if(e.keyCode === 13) {
+        $(this).trigger('enterKey');
+      }
+    });
+
+    $('#main-btn').click(runAction);
   });
 })(window);
